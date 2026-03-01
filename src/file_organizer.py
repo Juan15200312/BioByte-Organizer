32b84842b53df82a798f717a179368df8ce1732c
DIANACAROLINA2421 <carolinapita70@gmail.com>
2026-03-01 19:43:59 +0100

    nuevoCambio

--- src/file_organizer.py
+++ src/file_organizer.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from cryptography.fernet import Fernet
 
 
-class FileOrganizer:
+
+class OrganizarArchivos:
     def __init__(self):
         self.base_path = ""
         self.rules = {
@@ -22,30 +23,30 @@ class FileOrganizer:
             ".ogg": "Música",
             ".wav": "Música",
         }
-        self.backup_enabled = False
-        self.encrypt_enabled = False
-        self.sort_by = "carpeta"
+        self.respaldo_activado = False
+        self.encriptado_activado = False
+        self.ordenar_por = "carpeta"
 
         self.key = None
-        self.cipher = None
+        self.cifrar = None
 
 
-    def set_base_path(self, path):
+    def set_ruta_base(self, path):
         self.base_path = path
 
-    def enable_backup(self, value: bool):
-        self.backup_enabled = value
+    def activar_respaldo(self, value: bool):
+        self.respaldo_activado = value
 
-    def enable_encryption(self, value: bool):
-        self.encrypt_enabled = value
+    def activar_encriptado(self, value: bool):
+        self.encriptado_activado = value
         if value:
             self.key = Fernet.generate_key()
-            self.cipher = Fernet(self.key)
+            self.cifrar = Fernet(self.key)
 
-    def set_sorting(self, mode):
-        self.sort_by = mode
+    def set_ordenar(self, mode):
+        self.ordenar_por = mode
 
-    def scan_files(self):
+    def escanear_archivos(self):
         if not self.base_path:
             return []
 
@@ -54,13 +55,13 @@ class FileOrganizer:
             if os.path.isfile(os.path.join(self.base_path, f))
         ]
 
-        if self.sort_by == "fecha":
+        if self.ordenar_por == "fecha":
             files.sort(
                 key=lambda f: os.path.getmtime(os.path.join(self.base_path, f)),
                 reverse=False
             )
 
-        elif self.sort_by == "tamano":
+        elif self.ordenar_por == "tamano":
             files.sort(
                 key=lambda f: os.path.getsize(os.path.join(self.base_path, f))
             )
@@ -70,54 +71,56 @@ class FileOrganizer:
 
         return files
 
-    def get_destination_folder(self, filename):
-        ext = os.path.splitext(filename)[1].lower()
+    def get_carpeta_destino(self, nombre_archivo):
+        ext = os.path.splitext(nombre_archivo)[1].lower()
         return self.rules.get(ext, "Otros")
 
-    def backup_file(self, filepath):
-        backup_dir = os.path.join(self.base_path, "Backup")
-        os.makedirs(backup_dir, exist_ok=True)
-        shutil.copy(filepath, backup_dir)
+    def respaldar_archivo(self, ruta_archivo):
+        carpeta_respaldo= os.path.join(self.base_path, "Backup")
+        os.makedirs(carpeta_respaldo, exist_ok=True)
+        shutil.copy(ruta_archivo, carpeta_respaldo)
 
-    def encrypt_file(self, filepath):
-        with open(filepath, "rb") as f:
+    def encriptar_archivo(self, ruta_archivo):
+        with open(ruta_archivo, "rb") as f:
             data = f.read()
-        encrypted = self.cipher.encrypt(data)
-        with open(filepath, "wb") as f:
-            f.write(encrypted)
+        datos_encriptados = self.cifrar.encrypt(data)
+        with open(ruta_archivo, "wb") as f:
+            f.write(datos_encriptados)
 
-    def process_file(self, filename):
-        src = os.path.join(self.base_path, filename)
-        dest_folder = os.path.join(self.base_path, self.get_destination_folder(filename))
-        os.makedirs(dest_folder, exist_ok=True)
-        dest = os.path.join(dest_folder, filename)
+    def procesar_archivo(self, nombre_archivo):
+        origen = os.path.join(self.base_path, nombre_archivo)
+        carpeta_destino = os.path.join(self.base_path, self.get_carpeta_destino(nombre_archivo))
+        os.makedirs(carpeta_destino, exist_ok=True)
+        dest = os.path.join(carpeta_destino, nombre_archivo)
 
         try:
-            if self.backup_enabled:
-                self.backup_file(src)
+            if self.respaldo_activado:
+                self.respaldar_archivo(origen)
 
-            if self.encrypt_enabled:
-                self.encrypt_file(src)
+            if self.encriptado_activado:
+                self.encriptar_archivo(origen)
 
-            shutil.move(src, dest)
+            shutil.move(origen, dest)
 
             return {
-                "archivo": filename,
-                "destino": dest_folder,
+                "archivo": nombre_archivo,
+                "destino": carpeta_destino,
                 "fecha": datetime.now().strftime("%Y-%m-%d %H:%M"),
                 "estado": "OK"
             }
 
         except Exception as e:
             return {
-                "archivo": filename,
+                "archivo": nombre_archivo,
                 "destino": "-",
                 "fecha": datetime.now().strftime("%Y-%m-%d %H:%M"),
                 "estado": f"ERROR: {str(e)}"
             }
 
     def run(self):
-        results = []
-        for f in self.scan_files():
-            results.append(self.process_file(f))
-        return results
+        resultados = []
+        for f in self.escanear_archivos():
+            resultados.append(self.procesar_archivo(f))
+        return resultados
+
+
